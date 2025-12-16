12357b8b1348586110c140f969db98ec47788ceb
Aditya Parikh <aditya.m.parikh@gmail.com>
2025-12-16 15:04:42 -0500

    feat: Add security and docs  (#17)

--- src/main/java/org/apache/solr/mcp/server/indexing/IndexingService.java
+++ src/main/java/org/apache/solr/mcp/server/indexing/IndexingService.java
@@ -16,9 +16,6 @@
  */
 package org.apache.solr.mcp.server.indexing;
 
-import java.io.IOException;
-import java.util.List;
-import javax.xml.parsers.ParserConfigurationException;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
@@ -28,6 +25,10 @@ import org.springaicommunity.mcp.annotation.McpToolParam;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Spring Service providing comprehensive document indexing capabilities for
